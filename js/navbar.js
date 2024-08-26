0efbe717eac5c852484d37adf0743f659507eb7d
PennyH0W <160952834+PennyH0W@users.noreply.github.com>
2024-08-26 15:17:31 -0400

    Add files via upload

--- js/navbar.js
+++ js/navbar.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 document.getElementById('navbar').innerHTML = `
  <nav class="navbar navbar-expand-md navbar-light bg-white">
 
@@ -57,3 +58,71 @@ document.getElementById('navbar').innerHTML = `
     </div>
   </nav>
 `;
+=======
+document.getElementById('navbar').innerHTML = `
+ <nav class="navbar navbar-expand-md navbar-light bg-white">
+
+    <div class="container">
+        <!-- Brand Logo Positioned on the Left -->
+        <a class="navbar-brand brand-logo" href="index.php">
+            <img src="images/IAAYB-logo-2.png" alt="images/IAAYB-logo-2" width="40" height="40"> It's All About<br>Your Belly
+        </a>
+
+        <!-- Navbar Toggler Button for the Hambugrer Icon -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <!-- Navigation Items Positioned on the Right -->
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+
+                <li class="nav-item">
+                <a class="nav-link active" href="index.html">Home</a>
+                </li>
+                <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="aboutDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    About
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="aboutDropdown">
+                    <li><a class="dropdown-item" href="overview.html">Overview</a></li>
+                    <li><a class="dropdown-item" href="benefits.html">Benefits</a></li>
+                    <li><a class="dropdown-item" href="testimonials.html">Testimonials</a></li>
+                </ul>
+                </li>
+
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="classesDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Classes
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="servicesDropdown">
+                <li><a class="dropdown-item" href="liveClasses.html">Live Classes</a></li>
+                <li><a class="dropdown-item" href="zoomClasses.html">Zoom Classes</a></li>
+                <li><a class="dropdown-item" href="privateLessons.html">Private Lessons</a></li>
+                </ul>
+            </li>
+
+                <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="galleryDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Gallery
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="galleryDropdown">
+                <li><a class="dropdown-item" href="Featured.html">Featured</a></li>
+                <li><a class="dropdown-item" href="FantasyFest.html">Fanatsy Fest</a></li>
+                <li><a class="dropdown-item" href="Performances.html">Performances</a></li>
+                </ul>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="Zoom.html">Zoom</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="contact.html">Contact</a>
+            </li>
+            </ul>
+        </div>
+    </div>
+  </nav>
+`;
+>>>>>>> 1e2eb0f (Updated all pages with design, content, images,)
